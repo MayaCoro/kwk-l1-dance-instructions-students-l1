37c0f66844d849d69dd1e89d1046c312bbe850d5
MayaCoro <mayascoro@gmail.com>
2018-06-04 23:15:36 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,5 +2,5 @@ def Gangnam Style
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
   puts "Lift right foot"
   puts "Return right foot to the ground"
-  puts ""
+  puts "Finishing with a small skip-step backward"
 # Code your methods below
